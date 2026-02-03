41155242d497ba1089c8f19c81f484591d50dc01
pierre.hamel <pierre1754@outlook.com>
2026-02-03 22:33:10 +0100

    fix: font import

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <main className="min-h-screen w-full flex flex-col items-center justify-center gap-24">
-        <h1 className="text-8xl uppercase">Select your car brand</h1>
+        <h1 className="text-8xl uppercase font-bold">Select your car brand</h1>
 
         <ul className="flex flex-row gap-40">
           {brands.then((brandsData) =>
