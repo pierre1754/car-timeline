f070e022b30eca45aa5bef5ac838dae591931a7b
pierre.hamel <pierre1754@outlook.com>
2026-02-03 21:33:39 +0100

    feat: added theme

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans border">
       <main className="flex min-h-screen w-full flex-col items-center justify-between border">
-        <h1 className="text-9xl uppercase">Select your car</h1>
+        <h1 className="text-9xl font-extrabold uppercase">Select your car</h1>
 
         <ul className="">
           <li className="">Car 1 - 2020</li>
