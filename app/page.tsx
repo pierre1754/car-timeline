6fc7fed05610a6046fe714148583421ca5e33a62
pierre.hamel <pierre1754@outlook.com>
2026-02-20 14:16:22 +0100

    feat: improved background

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const brands = await fetchBrands();
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[url('/images/background.png')] bg-cover">
+    <div className="min-h-screen flex items-center justify-center bg-[url('/images/background.png')] bg-cover bg-center bg-background bg-blend-multiply">
       <main className="min-h-screen w-full flex flex-col items-center justify-center gap-24">
         <h1 className="text-8xl uppercase font-bold">Select your car brand</h1>
 
